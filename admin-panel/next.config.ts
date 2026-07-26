9348a12c3b79b0551756ddcaeb590ac3d857778d
MaruJigar <jigarmaru31@gmail.com>
2026-07-26 16:24:47 +0530

    Updated migration

--- admin-panel/next.config.ts
+++ admin-panel/next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'api.vikas-marketing.work.gd',
+        hostname: 'api.avchousehold.com',
       },
       {
         protocol: 'https',
